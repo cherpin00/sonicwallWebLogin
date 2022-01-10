67892f53168aa8f99d5b6ac8bd0d2dc602fe5838
cherpin00 <caleb.herpin@gmail.com>
2022-01-09 22:00:14 -0600

    Removed old programs

--- main.py
+++ main.py
@@ -1,136 +1,165 @@
 import time
-import re
-from hashlib import md5
-import requests
-from html.parser import HTMLParser
+import os
+from tkinter import *
+from tkinter import messagebox
+from multiprocessing import Process
+from selenium.common.exceptions import WebDriverException
+import configparser
+import io
 
-"""
-param1=C31F72D442591AC627E091E49918DEEB
-param2=777F1788DDBD2141A6E87925B0ABD4F6
-id=ed
-sessId=4556063AE86AE70E75CA35F06E5B2B06
-select2=English
-uName=admin
-pass=
-digest=530c360c8997065615f1123f6805610e
-"""
+from functions import HEADLESS, USERNAME, get_driver, login_and_set_time, logout
 
-# Enter your username and password
-UNAME = 'admin '
-PASSWORD = 'password71'
+class App:
+	def __init__(self):
+	# creating Tk window
+		get_driver()
 
-BEAT_INTERVAL = 15
-MIN_RELOGIN = 10
+		self.root = Tk()
 
-DOMAIN = 'http://webauth.login'
-LOGIN_PORTAL = DOMAIN + "/auth1.html"
-AUTH_PAGE = DOMAIN + '/auth.cgi'
-HEARTBEAT = DOMAIN + '/usrHeartbeat.cgi'
-LOGIN_STATUS = DOMAIN + '/loginStatusTop.html'
-DYN_LOGIN_STATUS = DOMAIN + '/dynLoginStatus.html?1stLoad=yes'
-LOGOUT = DOMAIN + '/dynLoggedOut.html?didLogout=yes'
+		self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
 
-class InputFieldParser(HTMLParser):
-    def handle_starttag(self, tag, attr_pairs):
-        if tag != 'input':
-            return
-        attr_dict = dict(attr_pairs)
-        if not 'name' in attr_dict:
-            return
-        if not 'value' in attr_dict:
-            return
-        name = attr_dict['name']
-        value = attr_dict['value']
-        if name == 'sessId':
-            print('Session ID,', value)
-            self.sess_id = value
-        elif name == 'param1':
-            print( 'Random param1,', value)
-            self.param1 = value
-        elif name == 'param2':
-            print( 'Random param2,', value)
-            self.param2 = value
-        elif name == 'id':
-            print('ID,',  value)
-            self.rid = value
+		self.durration = 1
+		self.isConnected=False
+		self.root.after(int((1 * 1000 * 60) / 2), self.task)
+		
+		# setting geometry of tk window
+		self.root.geometry("300x250")
 
-def bake_cookies(p):
-    cookies = {}
-    cookies['SessId'] = p.sess_id
-    page_seed = md5((PASSWORD + p.param2).encode()).hexdigest()
-    print('PageSeed, ' + page_seed)
-    cookies['PageSeed'] = page_seed
-    # Dunno?
-    cookies['temp'] = 'temp'
-    return cookies
+		# Using title() to display a message in
+		# the dialogue box of the message in the
+		# title bar.
+		self.root.title("Time Counter")
 
-def make_form(p):
-    form = {}
-    form['param1'] = p.param1
-    form['param2'] = p.param2
-    form['id'] = p.rid
-    form['sessId'] = p.sess_id
-    form['select2'] = 'English'
-    form['uName'] = UNAME
-    form['pass'] = ''
-    form['digest'] = md5((p.rid + PASSWORD + p.param1).encode()).hexdigest()
-    print("digest is", form['digest'])
-    return form
+		# Declaration of variables
+		# self.hour=StringVar()
+		# self.minute=StringVar()
+		# self.second=StringVar()
+		self.status = StringVar()
 
-def req_login_page():
-    # Disable SSL cert verification
-    resp = requests.get(LOGIN_PORTAL, verify = False)
-    parser = InputFieldParser()
-    parser.feed(resp.text)
-    return parser
+		# setting the default value as 0
+		# self.hour.set("00")
+		# self.minute.set("00")
+		# self.second.set("00")
+		self.status.set("Not connected")
 
-def parse_beat(data):
-    val = re.findall('var remTime=(\d*);', data)[0]
-    return int(val)
+		self.statusLabel = Label(self.root, textvariable=self.status)
+		self.statusLabel.pack()
 
-def do_login():
-    # p = req_login_page()
-    p = InputFieldParser()
-    p.param1="C31F72D442591AC627E091E49918DEEB"
-    p.param2="777F1788DDBD2141A6E87925B0ABD4F6"
-    p.rid="ed"
-    p.sess_id="4556063AE86AE70E75CA35F06E5B2B06"
-    # p.select2="English"
-    # p.uName="admin"
-    # p.pass=""
-    # p.digest="530c360c8997065615f1123f6805610e"
-    print('Got login page')
-    cookies = bake_cookies(p)
-    form = make_form(p)
-    print(form)
-    return
-    login_req = requests.post(AUTH_PAGE, data = form, cookies = cookies, verify = False)
-    if login_req.status_code != 200:
-        raise Exception('Error in logging in')
-    # TODO: How do we handle the redirect
-    if login_req.text.find('auth.html') != -1:
-        raise Exception('Invalid credentials/Already logged in')
-    print('Logged in successfully')
-    requests.get(LOGIN_STATUS, cookies = cookies, verify = False)
-    requests.get(DYN_LOGIN_STATUS, cookies = cookies, verify = False)
-    return cookies
+		self.userLabel = Label(self.root, text=USERNAME)
+		self.userLabel.pack()
+		# Use of Entry class to take input from the user
+		# self.hourEntry= Entry(self.root, width=3, font=("Arial",18,""),
+		# 				textvariable=self.hour)
+		# self.hourEntry.place(x=80,y=20)
+
+		# self.minuteEntry= Entry(self.root, width=3, font=("Arial",18,""),
+		# 				textvariable=self.minute)
+		# self.minuteEntry.place(x=130,y=20)
+
+		# self.secondEntry= Entry(self.root, width=3, font=("Arial",18,""),
+		# 				textvariable=self.second)
+		# self.secondEntry.place(x=180,y=20)
+
+			# button widget
+		self.connectButton = Button(self.root, text='Connect', bd='5',
+				command=self.connect)
+		self.connectButton.place(x = 70,y = 120)
+		
+		self.disconnectButton = Button(self.root, text='Disconnect', bd='5',
+				command=self.disconnect)
+		self.disconnectButton.place(x = 140,y = 120)
+	# def submit():
+	# 	try:
+	# 		# the input provided by the user is
+	# 		# stored in here :temp
+	# 		temp = int(hour.get())*3600 + int(minute.get())*60 + int(second.get())
+	# 	except:
+	# 		print("Please input the right value")
+	# 	while temp >-1:
+			
+	# 		# divmod(firstvalue = temp//60, secondvalue = temp%60)
+	# 		mins,secs = divmod(temp,60)
+
+	# 		# Converting the input entered in mins or secs to hours,
+	# 		# mins ,secs(input = 110 min --> 120*60 = 6600 => 1hr :
+	# 		# 50min: 0sec)
+	# 		hours=0
+	# 		if mins >60:
+				
+	# 			# divmod(firstvalue = temp//60, secondvalue
+	# 			# = temp%60)
+	# 			hours, mins = divmod(mins, 60)
+			
+	# 		# using format () method to store the value up to
+	# 		# two decimal places
+	# 		hour.set("{0:2d}".format(hours))
+	# 		minute.set("{0:2d}".format(mins))
+	# 		second.set("{0:2d}".format(secs))
+
+	# 		# updating the GUI window after decrementing the
+	# 		# temp value every time
+	# 		root.update()
+	# 		time.sleep(1)
+
+	# 		# when temp value = 0; then a messagebox pop's up
+	# 		# with a message:"Time's up"
+	# 		if (temp == 0):
+	# 			messagebox.showinfo("Time Countdown", "Time's up ")
+			
+	# 		# after every one sec the value of temp will be decremented
+	# 		# by one
+	# 		temp -= 1
+
+	def connect(self):
+		# self.connectProcess = Process(target=block_connect)
+		# self.connectProcess.start()
+		login_and_set_time(str(self.durration))
+		self.isConnected = True
+		self.status.set("Connected")
+
+	def disconnect(self):
+		# self.connectProcess.terminate()
+		self.isConnected = False
+		self.status.set("Not connected")
+		logout()
+
+	def start(self):
+		# infinite loop which is required to
+		# run tkinter program infinitely
+		# until an interrupt occurs
+		self.root.mainloop()
+
+	def task(self):
+		if self.isConnected:
+			login_and_set_time(str(self.durration))
+		self.root.after(int((1 * 1000 * 60) / 2), self.task)
+	
+	def on_closing(self):
+		# logout()
+		try:
+			print("Logging out...")
+			logout()
+		except WebDriverException:
+			print("WARNING: Failed to logout.  Chrome was not open.")
+		print("Closing...")
+		self.root.destroy()
+		pass
+			
 
 def main():
-    cookies = do_login()
-    return
-    try:
-        while True:
-            beat_req = requests.post(HEARTBEAT, cookies = cookies, verify = False)
-            rem_time = parse_beat(beat_req.text)
-            print('Heatbeat ..*..', rem_time)
-            if rem_time < MIN_RELOGIN:
-                print('Re-logging in...')
-                cookies = do_login()
-            time.sleep(BEAT_INTERVAL)
-    finally:
-        print('Exit. Logging out...')
-        requests.get(LOGOUT, verify = False)
+	global USERNAME, HEADLESS
+	configFile = "config.ini"
+	if os.path.exists(configFile):
+		configStr = '[DEFAULT]\n' + open(configFile, 'r').read()
+		configFP = io.StringIO(configStr)
+		config = configparser.RawConfigParser()
+		config.read_file(configFP)
+		USERNAME = config["DEFAULT"].get("user", USERNAME)
+		HEADLESS = config["DEFAULT"].get("HEADLESS", HEADLESS)
+	print("Username is", USERNAME)
+	print("Headless is", HEADLESS)
+	app = App()
+	app.start()
 
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+	main()
--- main3.py
+++
@@ -1,6 +0,0 @@
-from webbot import Browser 
-web = Browser()
-web.go_to('http://webauth.login') 
-web.type("cherpin", into="Username")
-web.type("P@ssword36", into="Password")
-r = web.click('Login')
